07a4ea5d4ab8c6221c36696ee409d3568675d6f0
staten09 <staten09@gmail.com>
2022-02-28 20:02:42 -0600

    23.8.1 and 23.8.2

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 // Write your JavaScript code here.
+
 // Remember to pay attention to page loading!
+window.addEventListener('load', function () {
+    let flightStatus = document.getElementById("flightStatus");
+    let bgFlightScreen = document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    let takeoffButton = document.getElementById("takeoff")
+    takeoffButton.addEventListener('click', event => {
+    let shuttleReady = window.confirm("Confirm that the shuttle is ready for takeoff.");
+    if(shuttleReady){
+        flightStatus.innerText = "Shuttle in flight.";
+        bgFlightScreen.style.backgroundColor = "blue";
+        spaceShuttleHeight.innerText = "10000";
+    }
+})
+})
