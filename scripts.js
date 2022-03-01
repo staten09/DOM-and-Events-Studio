14697182403760667777e6c665bc9fbdcbdc32c2
staten09 <staten09@gmail.com>
2022-02-28 20:24:51 -0600

    updated Land button functions

--- scripts.js
+++ scripts.js
@@ -14,4 +14,26 @@ window.addEventListener('load', function () {
         spaceShuttleHeight.innerText = "10000";
     }
 })
+
+When the "Land" button is clicked, the following should happen:
+A window alert should let the user know "The shuttle is landing. Landing gear engaged."
+The flight status should change to "The shuttle has landed."
+The background color of the shuttle flight screen should change from blue to green.
+The shuttle height should go down to 0.
+
+let landButton = document.getElementById("landing");
+landButton.addEventListener('click', function () {
+    let shuttleLanding = window.alert("The shuttle is landing. Landing gear engaged.")
+    let flightStatus = document.getElementById("flightStatus");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    let bgFlightScreen = document.getElementById("shuttleBackground");
+    if(shuttleLanding){
+        flightStatus.innerText = "The shuttle has landed.";
+        bgFlightScreen.style.backgroundColor = "green";
+        spaceShuttleHeight.innerText = "0";
+    }
+
+})
+
+
 })
